d01a608536331f6ff626b1f2ae523d2a7001b4b1
mcsay <mcsaygili@outlook.com>
2022-03-12 17:30:53 +0100

    2 script added

---
+++ concatination.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter 1st string"
+read str1
+
+echo "enter 2rd string"
+read str2
+
+c=$str1$str2
+
+echo "$c"
---
+++ upperandlowercase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter 1st string"
+read str1
+
+echo "enter 2rd string"
+read str2
+
+
+echo ${str1^}
+echo ${str1^^}
+
